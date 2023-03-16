d2b6e6cd86c419e29ec9ca9b8c897de712290695
Marina Nogovitsyna <97659223+MariNogo@users.noreply.github.com>
2023-03-16 18:25:44 +0900

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,35 @@
+class AuthEmail:
+    email = 'marinogov@gmail.com'
+    password = 'Crbkkafrnjhb23'
+
+
+class AuthPhone:
+    phone = '9142793243'
+    password = 'Crbkkafrnjhb23'
+
+
+class AuthLogin:
+    login = 'janitta'
+    password = 'Crbkkafrnjhb23'
+
+
+class InvalidData:
+    login = 'wronglog'
+    password = "crbkkafrnjhb23"
+
+
+class SymbolData:
+    login = '@#####@'
+    password = "crbkkafrnjhb23"
+
+
+class skillData:
+    login = 'ПОРУССКИ'
+    password = "ПОРУССКИ"
+
+
+class BIGData:
+    login = 'login'
+    password = "123"
+
+
